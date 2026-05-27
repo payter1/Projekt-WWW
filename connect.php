e71cf83fcb180e44b429d97c8fcdb6548c292691
payter1 <49399264+payter1@users.noreply.github.com>
2026-05-27 21:24:34 +0200

    .

--- connect.php
+++ connect.php
@@ -1,12 +1,8 @@
 <?php
-
-// $dbUsers = "(DESCRIPTION=(ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = 10.16.4.20)(PORT = 1521)))(CONNECT_DATA=(SID=sssw2)))";
-// $connUsers = oci_connect($_SESSION['login'], $_SESSION['pass'], $dbUsers, 'AL32UTF8');
-
 $host = "localhost";
 $db_user = "root";
 $db_password = "";
-$db_name = "oslydb";
+$db_name = "donkeysDB";
 //$connect = mysqli_connect($host,$db_user,$db_password,$db_name);
 $conn = new mysqli($host,$db_user,$db_password,$db_name);
 $conn->set_charset("UTF8");
