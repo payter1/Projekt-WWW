62b1d57218bc41a46113d36bd902903c78b17ba6
payter1 <49399264+payter1@users.noreply.github.com>
2026-06-12 23:34:05 +0200

    copyright

--- partials/footer.php
+++ partials/footer.php
@@ -1,5 +1,6 @@
 <footer class="bg-light text-white-50 pt-5 pb-3 mt-auto border-top border-secondary">
     <div class="container">
+        @ Copyright Piotr Bontruk
     </div>
 </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
